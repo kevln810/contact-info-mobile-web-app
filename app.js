18d7fb610cbc786e5fece0486f11b69fec6fe2fd
kenkmkm <125950184+kenkmkm@users.noreply.github.com>
2023-05-11 03:15:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function displayContacts(contacts) {
 }
 
 function addRow(contact) {
- var m = document.getElementById("tcontent");
+ var m= document.getElementById("tcontent");
   var mrow = m.insertRow();
    var namecell = mrow.insertCell();
    namecell.innerHTML = contact.name;
